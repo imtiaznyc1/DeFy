7e1b0cc56c7ee928ca8989a21f195441b4d14f8f
Imtiaz Rahman <32941495+imtiaznyc1@users.noreply.github.com>
2022-02-27 01:54:11 -0500

    home js file is updated

---
+++ home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+ 
+export default function HomeScreen(){
+    return(
+        //here we fill it in with the layout
+        <View>
+            <Text>YURRRRR</Text>
+        </View>
+        
+    )
+}
+
+
+
+
+
+
+
