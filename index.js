b7efa218e563345fa72f51a0ec3d31f59136166f
Imtiaz Rahman <32941495+imtiaznyc1@users.noreply.github.com>
2022-02-27 03:27:19 -0500

    testing 123

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { creatNativeStackNavigator} from '@react-navigation/native-stack'
+import { createNativeStackNavigator} from '@react-navigation/native-stack'
 import {HomeScreen} from home.js
 
 const Stack = createNativeStackNavigator();
-
+//i am alive
 const MyStack = () => {
     return (
         <NavigationContainer>
