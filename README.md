40091fe012c1e5940d90fa33220722b87cea8d78
Imtiaz Rahman <32941495+imtiaznyc1@users.noreply.github.com>
2022-02-27 01:15:48 -0500

    addedd updated index.js with stack and home js file

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 ![LOGO](/assets/Defy.png)
 ## HELLO USER!
 
+Welcome to DeFy bAnK, a decentralized form of managing and obtaining wealth. In a world of restricted 
+
 To run the app, ensure that you have the latest version of node js installed to have the following correct packages. Once node is in the system, run this command on terminal
 
 **npm install -g expo-cli**
---
+++ index.js
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { creatNativeStackNavigator} from '@react-navigation/native-stack'
+
+const Stack = createNativeStackNavigator();
+
+const MyStack = () => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen
+                    name = "Home"
+                    component = {HomeScreen}
+                />
+                <Stack.Screen name = "Loans" component = {ProfileScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
