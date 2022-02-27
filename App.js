c57ae533f7457b41cf5d7884bbc1fe2ccee8279f
Dipu Chowdhury <dipuchowdhury@dipus-mbp.myfiosgateway.com>
2022-02-27 04:13:50 -0500

    uppercase App.js

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator} from '@react-navigation/native-stack'
 import HomeScreen from './home'
+import InvestmentScreen from './investment'
 
 export default function App() {
  
@@ -14,6 +15,7 @@ export default function App() {
       //</View>
       <View>
         <HomeScreen />
+        <InvestmentScreen />
       </View>
       
     );
