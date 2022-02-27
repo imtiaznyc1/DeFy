aad683bb30d5cc2a42fb9b32a907d076422e5395
Dipu Chowdhury <dipuchowdhury@dipus-mbp.myfiosgateway.com>
2022-02-27 03:41:52 -0500

    initial investment page-editted

---
+++ investment.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const InvestmenScreen = () => {
+    return(
+        //here we fill it in with the layout
+        <View>
+            <Text>YURRRRR</Text>
+        </View>
+        
+    );
+}
+
+export default InvestmentScreen;
